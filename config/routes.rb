7bfb1f1a04c1f6e3574c1aefe4f7fa737af30ca9
Jingchao Lee <satzcoal@hotmail.com>
2015-09-09 01:59:25 +0800

    a

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,14 @@ HmHx6::Application.routes.draw do
   get "test" => 'tmp#stest'
   get "about" => 'tmp#about'
   get "contact" => 'tmp#contact'
+  get 'tech' => 'tmp#tech'
+  get 'company' => 'tmp#company'
+  get 'product' => 'tmp#product'
 
   get "products" => 'products#index'
   get "products/:id" => 'products#show'
 
   get "verify/:id" => 'verify_items#check'
 
-  root :to => 'home#index'
+  root :to => 'tmp#company'
 end
